0e5b61022f5c529fb2031ab49875fafb6a322dd3
Rensu Theart (MacStudio) <rensutheart22@gmail.com>
2022-04-21 15:57:49 +0200

    Update indent-blankline

--- config.lua
+++ config.lua
@@ -209,17 +209,20 @@ lvim.plugins = {
     "lukas-reineke/indent-blankline.nvim",
     event = "BufRead",
     setup = function()
-      require("indent_blankline").setup({
-        show_current_context = true,
-        show_current_context_start = true,
-
-      })
-      vim.g.indentLine_enabled = 1
+      vim.g.indent_blackline_enabled = 1
       -- vim.g.indent_blankline_char = "?"
       vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
       vim.g.indent_blankline_buftype_exclude = { "terminal" }
       vim.g.indent_blankline_show_trailing_blankline_indent = false
       vim.g.indent_blankline_show_first_indent_level = false
+      vim.g.indent_blankline_show_current_context = true
+      vim.g.indent_blankline_show_current_context_start = true
+      -- require("indent_blankline").setup({
+      --   show_current_context = true,
+      --   show_current_context_start = true,
+
+      -- })
+
     end,
   },
   {
